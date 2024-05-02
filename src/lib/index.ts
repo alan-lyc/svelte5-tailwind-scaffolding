e11b382404a75c51f562a4d595b093569f277873
alan-lyc <74176167+alan-0059bfc579@users.noreply.github.com>
2024-05-02 22:56:29 +0800

    add re-exports and bump version

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
 // Reexport your entry components here
 export { default as Scaffolding } from './components/Scaffolding.svelte';
 export { default as Button } from './components/Button.svelte';
+export { default as BaseModal } from './components/BaseModal.svelte';
+export { default as ModalContent } from './components/ModalContent.svelte';
 export { modal, block } from './modal.svelte.js';
